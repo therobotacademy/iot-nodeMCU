bdcbad3330ac657d56562a8ef525f19fa8544583
brjapon <brjapon@therobotacademy.com>
2018-05-04 21:05:42 +0200

    SmartConfig for ESP8266 using Android app

---
+++ 04_SmartConfig/04_SmartConfig.ino
@@ -0,0 +1,35 @@
+#include <ESP8266WiFi.h>;
+
+#define LED D4            // GPIO2 (D4), it is also the built in led
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+  Serial.begin(115200);
+  delay(10);
+
+  WiFi.mode(WIFI_AP_STA);
+  delay(2000);
+  Serial.println(".Hello.");
+  WiFi.beginSmartConfig();
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    Serial.println(WiFi.smartConfigDone());
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  delay(2000);
+  Serial.println(WiFi.status());
+  Serial.println(WiFi.SSID());
+  if (WiFi.status() == WL_CONNECTED)
+    digitalWrite(LED, HIGH);
+  else digitalWrite(LED, LOW);
+}
